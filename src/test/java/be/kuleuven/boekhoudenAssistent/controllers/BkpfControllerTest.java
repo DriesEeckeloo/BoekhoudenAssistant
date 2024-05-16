3e5e8c21918a4d2906ddd135848af43b0e45f009
Dries <Dries@>
2024-05-16 15:11:56 +0200

    testing

--- src/test/java/be/kuleuven/boekhoudenAssistent/controllers/BkpfControllerTest.java
+++ src/test/java/be/kuleuven/boekhoudenAssistent/controllers/BkpfControllerTest.java
@@ -3,6 +3,7 @@ package be.kuleuven.boekhoudenAssistent.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -10,6 +11,7 @@ import static org.springframework.test.jdbc.JdbcTestUtils.countRowsInTable;
 import static org.springframework.test.jdbc.JdbcTestUtils.countRowsInTableWhere;
 ;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,6 +20,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class BkpfControllerTest extends AbstractTransactionalJUnit4SpringContextTests {
 
     private final MockMvc mockMvc;
+    private static final String jsonData ="{\"GJAHR\": \"\",\"BUKRS\": \"\",\"BELNR1\": \"\",\"BELNR2\": \"\"}";
+
 
     public BkpfControllerTest(MockMvc mockMvc) {
         this.mockMvc = mockMvc;
@@ -26,6 +30,26 @@ public class BkpfControllerTest extends AbstractTransactionalJUnit4SpringContext
     @Test
     void getAllBkpf() throws Exception {
         mockMvc.perform(get("/bkpf"))
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.length()").value(countRowsInTable("BKPF")));
+    }
+
+    @Test
+    void getAllBkpfWhere() throws Exception {
+        mockMvc.perform(post("/bkpf/filter")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonData))
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.length()").value(countRowsInTable("BKPF")));
+    }
+
+    @Test
+    void getAllBkpfWhereWithDetails() throws Exception {
+        mockMvc.perform(post("/bkpf/filter/details")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonData))
                 .andExpectAll(
                         status().isOk(),
                         jsonPath("$.length()").value(countRowsInTable("BKPF")));
